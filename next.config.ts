7830f5b8628777bd7863501d530f802d72a6b902
kasunprabashwara <kasunprabashwara24@gmail.com>
2025-01-16 15:31:38 +0530

    disabled eslint on build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ const nextConfig = {
   basePath: "/portfolio",
   output: "export",
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }
 };
 
 module.exports = nextConfig;
