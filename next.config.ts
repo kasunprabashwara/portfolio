90985476e21d2f0175e224cfd27251fbc4d31637
kasunprabashwara <kasunprabashwara24@gmail.com>
2025-01-16 17:39:33 +0530

    removed readme.md

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
